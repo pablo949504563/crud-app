d9655f4e1142175bae427f7ada50e6ee0f5545e2
Pablo Gonzalez <pablo949504563@gmail.com>
2023-11-04 22:45:11 -0300

    Commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'crud-app';
+  constructor(private _dialog: MatDialog){}
+  openAddEditEmpForm(){
+    this._dialog.open(EmpAddEditComponent);
+  }
+}
